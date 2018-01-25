a5de04f33b58de84e0b9182869fae3e4645059a9
cumthyb <cumthyb@163.com>
2018-01-25 10:23:16 +0800

    添加基于React-Quill的富文本编辑组件 richtexteditor

---
+++ src/blog/components/richtexteditor/editor.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './index.css';
+
+class RichTextEditor extends Component {
+    constructor() {
+        super();
+        this.state = {
+            editorHtml: '',
+            theme: 'snow'
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(html) {
+        console.log(html);
+        this.setState({
+            editorHtml: html
+        });
+    }
+
+    modules = {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+            ['blockquote', 'code-block'],
+
+            [{
+                'header': 1
+            }, {
+                'header': 2
+            }], // custom button values
+            [{
+                'list': 'ordered'
+            }, {
+                'list': 'bullet'
+            }],
+            [{
+                'script': 'sub'
+            }, {
+                'script': 'super'
+            }], // superscript/subscript
+            [{
+                'indent': '-1'
+            }, {
+                'indent': '+1'
+            }], // outdent/indent
+            [{
+                'direction': 'rtl'
+            }], // text direction
+
+            [{
+                'size': ['small', false, 'large', 'huge']
+            }], // custom dropdown
+            [{
+                'header': [1, 2, 3, 4, 5, 6, false]
+            }],
+
+            [{
+                'color': []
+            }, {
+                'background': []
+            }], // dropdown with defaults from theme
+            [{
+                'font': []
+            }],
+            [{
+                'align': []
+            }],
+            ['link', 'image', 'video'],
+
+            ['clean']
+        ],
+        clipboard: {
+            // toggle to add extra line breaks when pasting HTML:
+            matchVisual: false,
+        }
+    }
+
+    /* 
+     * Quill editor formats
+     * See https://quilljs.com/docs/formats/
+     */
+    formats =[
+        'header', 'font', 'size', 'background', 'color', 'script', 'underline',
+        'bold', 'italic', 'underline', 'strike', 'code',
+        'list', 'bullet', 'indent', 'align', 'direction', 'header', 'blockquote', 'code-block',
+        'link', 'image', 'video'
+    ];
+
+    render() {
+        return (
+            <ReactQuill theme={ this.state.theme } onChange={ this.handleChange } value={ this.state.editorHtml } modules={ this.modules } formats={ this.formats } bounds={ '.app' }
+              placeholder={ "RichTextEditor---react-quill" } />
+            );
+    }
+}
+
+
+export default RichTextEditor;
---
+++ src/blog/components/richtexteditor/htmlviewer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class HtmlViewer extends Component {
+
+    render() {
+        return (
+            <div className="quill">
+              <div className="ql-container ql-snow" style={ { border: "none" } }>
+                <div className="ql-editor">
+                  <div dangerouslySetInnerHTML={ { __html: this.props.editorHtml } }></div>
+                </div>
+              </div>
+            </div>
+            );
+    }
+}
+
+export default HtmlViewer;
